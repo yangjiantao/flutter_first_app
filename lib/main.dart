ed79ad31bb900d68221e383616c7937fff298f87
yangjiantao7 <cdyangjiantao1@jd.com>
2021-10-04 22:48:32 +0800

    feat(pageView): PageController简单使用

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,8 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: PageViewSimple(),
     );
   }
 }
--- lib/widget/pageview_simple.dart
+++ lib/widget/pageview_simple.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 ///
@@ -22,26 +21,40 @@ class _PageViewSimpleState extends State<PageViewSimple> {
   var height;
   late List<Color> _colors;
 
+  // controller 使用, 设置视图缩放比例和默认显示页面位置
+  var _viewportFraction = 0.6; // 视图缩放比
+  var _pageCtrl; // 页面控制器
+  var _initOffset = 1;
+
   @override
   void initState() {
     // 初始化
     _colors = [Colors.red, Colors.yellow, Colors.blue, Colors.green, Colors.black, Colors.purple];
+    _pageCtrl = PageController(viewportFraction: _viewportFraction, initialPage: _initOffset);
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
     // 不设置组件width时，使用屏幕宽度
-    width = widget.width ?? MediaQuery.of(context).size.width;
+    width = widget.width ?? MediaQuery
+        .of(context)
+        .size
+        .width;
     height = widget.height ?? 120.0;
-    return Container(
-      width: width,
-      height: height,
-      child: PageView.builder(
-          scrollDirection: Axis.horizontal, // 方向
-          itemCount: _colors.length,
-          itemBuilder: (ctx, i) => createItemView(_colors, i)),
-    );
+    // flutter Text底部黄色双划线 问题:
+    // https://www.jianshu.com/p/6278b0867b98
+    return Material(
+        type: MaterialType.transparency,
+        child: Container(
+          width: width,
+          height: height,
+          child: PageView.builder(
+              scrollDirection: Axis.horizontal, // 方向
+              itemCount: _colors.length,
+              controller: _pageCtrl,
+              itemBuilder: (ctx, i) => createItemView(_colors, i)),
+        ));
   }
 
   /// 创建PageView itemView
