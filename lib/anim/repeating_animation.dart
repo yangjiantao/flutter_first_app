664ad16221efc79e6c2ed01b67eb6e5589b31f1b
yangjiantao <jiantaoyoung@gmail.com>
2021-10-30 22:36:46 +0800

    feat: 带阴影圆形加动画

---
+++ lib/anim/repeating_animation.dart
@@ -0,0 +1,72 @@
+// Copyright 2019 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+
+class RepeatingAnimationDemo extends StatefulWidget {
+  const RepeatingAnimationDemo({Key? key}) : super(key: key);
+  static String routeName = '/misc/repeating_animation';
+
+  @override
+  RepeatingAnimationDemoState createState() => RepeatingAnimationDemoState();
+}
+
+class RepeatingAnimationDemoState extends State<RepeatingAnimationDemo>
+    with SingleTickerProviderStateMixin {
+  late final AnimationController _controller;
+
+  // late final Animation<BorderRadius?> _borderRadius;
+  late final Animation<int> _sizeAnim;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controller = AnimationController(duration: const Duration(seconds: 2), vsync: this)
+      ..repeat(reverse: true);
+
+    _sizeAnim = IntTween(begin: 200, end: 160).animate(_controller);
+    // _borderRadius = BorderRadiusTween(
+    //   begin: BorderRadius.circular(100.0),
+    //   end: BorderRadius.circular(0.0),
+    // ).animate(_controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Repeating Animation')),
+      body: Center(
+        child: AnimatedBuilder(
+          animation: _sizeAnim,
+          builder: (context, child) {
+            return Stack(
+              children: [
+                Container(
+                  width: _sizeAnim.value.toDouble(),
+                  height: _sizeAnim.value.toDouble(),
+                  decoration:
+                      BoxDecoration(color: Colors.black26, shape: BoxShape.circle, boxShadow: [
+                    BoxShadow(
+                      color: Colors.black45,
+                      // offset: Offset(1,1),
+                      blurRadius: 5,
+                      // spreadRadius: 1
+                    )
+                  ]),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+}
